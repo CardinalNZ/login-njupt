ac4df1303e4c7b0ce9685992ecb771caea1a7d9e
CardinalNZ <60724684+CardinalNZ@users.noreply.github.com>
2024-10-25 21:43:02 +0800

    添加图片+修改脚本格式

--- login_njupt.sh
+++ login_njupt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #定义变量
-login_IP='校园网登录IP'
-not_sign_in_title='上网登录页'
-result_return='"result":1'
-sign_parameter='请求URL的文本链接'
-signed_in_title='注销页'
+login_IP="https://p.njupt.edu.cn/a79.htm"
+not_sign_in_title="上网登录页"
+result_return="\"result\":1"
+sign_parameter="https://p.njupt.edu.cn:802/eportal/portal/login?callback=dr1003&login_method=1&user_account=%2C0%2C1222067026%40njxy&user_password=Zhujihong%400673&wlan_user_ip=10.161.239.24&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&jsVersion=4.1.3&terminal_type=1&lang=zh-cn&v=5807&lang=zh"
+signed_in_title="注销页"
 
 # 发送HTTP请求，尝试访问登录页面
 req=$(curl -s --max-time 1 "$login_IP" | iconv -f gb2312 -t utf-8)
@@ -33,3 +33,4 @@ fi
 # 未连接到校园网的情况
 echo "未连接到校园网"
 exit 0
+
